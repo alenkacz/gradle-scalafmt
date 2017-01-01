c32ec34b173cd6904904f8750ee2e0ccedb527c3
Alena Varkockova <varkockova.a@gmail.com>
2017-01-01 16:35:43 +0100

    Missing files

---
+++ src/main/groovy/cz/alenkacz/gradle/scalafmt/ConfigFactory.groovy
@@ -0,0 +1,24 @@
+package cz.alenkacz.gradle.scalafmt
+
+import org.gradle.api.Project
+import org.gradle.api.logging.Logger
+import org.scalafmt.config.Config
+import org.scalafmt.config.ScalafmtConfig
+import org.scalafmt.config.ScalafmtConfig$
+
+class ConfigFactory {
+    static ScalafmtConfig load(Logger logger, Project project) {
+        def customConfig = project.file(".scalafmt.conf")
+        if (!customConfig.exists()) {
+            logger.info("Custom config not found, using default scalafmt config")
+            return ScalafmtConfig$.MODULE$.default
+        }
+        def parsedConfig = Config.fromHocon(customConfig.text, Config.fromHocon$default$2())
+        if (parsedConfig.right) {
+            parsedConfig.right().get()
+        } else {
+            logger.warn("Unable to parse .scalafmt.conf in the project root. The format of the file is incorrect.")
+            ScalafmtConfig$.MODULE$.default
+        }
+    }
+}
---
+++ src/test/groovy/cz/alenkacz/gradle/scalafmt/ProjectMother.groovy
@@ -0,0 +1,55 @@
+package cz.alenkacz.gradle.scalafmt
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class ProjectMother {
+    static def basicProject() {
+        TestProject testProject = null
+        File.createTempDir().with {
+            deleteOnExit()
+            def srcFolder = new File(absoluteFile, sourceFilePath)
+            srcFolder.mkdirs()
+            def srcFile = Files.createFile(Paths.get(srcFolder.absolutePath, "Test.scala"))
+            srcFile.write testSourceFile
+            testProject = new TestProject(absoluteFile, srcFile.toFile())
+        }
+        return testProject
+    }
+
+    private static def testSourceFile = """import java.nio.file.{Paths, Files}
+                                           |object Test { foo(a, // comment
+                                           |    b)}
+                                           """.stripMargin()
+    private static def sourceFilePath = "src/main/scala/cz/alenkacz/gradle/scalafmt/test"
+
+    static def projectWithConfig() {
+        TestProject testProject = null
+        File.createTempDir().with {
+            deleteOnExit()
+            def configFile = Files.createFile(Paths.get(absolutePath, ".scalafmt.conf"))
+            configFile.write "rewrite.rules = [SortImports]"
+            def srcFolder = new File(absoluteFile, sourceFilePath)
+            srcFolder.mkdirs()
+            def srcFile = Files.createFile(Paths.get(srcFolder.absolutePath, "Test.scala"))
+            srcFile.write testSourceFile
+            testProject = new TestProject(absoluteFile, srcFile.toFile())
+        }
+        return testProject
+    }
+
+    static def projectWithBadllyFormattedConfig() {
+        TestProject testProject = null
+        File.createTempDir().with {
+            deleteOnExit()
+            def configFile = Files.createFile(Paths.get(absolutePath, ".scalafmt.conf"))
+            configFile.write "badconfig 123"
+            def srcFolder = new File(absoluteFile, sourceFilePath)
+            srcFolder.mkdirs()
+            def srcFile = Files.createFile(Paths.get(srcFolder.absolutePath, "Test.scala"))
+            srcFile.write testSourceFile
+            testProject = new TestProject(absoluteFile, srcFile.toFile())
+        }
+        return testProject
+    }
+}
