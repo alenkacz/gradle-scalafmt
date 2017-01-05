99e528f0dc2c1f0723500e290cb4fce01a586095
Jan Strnad <strnad@avast.com>
2017-01-05 14:42:17 +0100

    Use GradleException instead of Exception

--- src/main/groovy/cz/alenkacz/gradle/scalafmt/ScalafmtFormatException.groovy
+++ src/main/groovy/cz/alenkacz/gradle/scalafmt/ScalafmtFormatException.groovy
@@ -1,6 +1,8 @@
 package cz.alenkacz.gradle.scalafmt
 
-class ScalafmtFormatException extends Exception {
+import org.gradle.api.GradleException
+
+class ScalafmtFormatException extends GradleException {
     public ScalafmtFormatException(List<String> filePaths) {
         super("Files incorrectly formatted: " + filePaths.join(","))
     }
