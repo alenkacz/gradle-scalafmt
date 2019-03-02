287eadb48561a23ea92214f106e76450c42d9b53
Boris Smidt <smidtboris1@gmail.com>
2019-03-02 08:46:23 +0100

    Updated the project so it uses the dynamic (#25)
    loaded version of the formatter instead of a fixed version.

--- src/main/groovy/cz/alenkacz/gradle/scalafmt/ConfigFactory.groovy
+++ src/main/groovy/cz/alenkacz/gradle/scalafmt/ConfigFactory.groovy
@@ -2,28 +2,17 @@ package cz.alenkacz.gradle.scalafmt
 
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
-import org.scalafmt.config.Config
-import org.scalafmt.config.ScalafmtConfig
-import org.scalafmt.config.ScalafmtConfig$
 
 class ConfigFactory {
-    static ScalafmtConfig load(Logger logger, Project project, String configFilePath) {
+    static File get(Logger logger, Project project, String configFilePath) {
         def customConfig = project.file(configFilePath)
         if (!customConfig.exists()) {
             // when config does not exist in the project folder, look also to a root of multimodule project
             customConfig = project.rootProject.file(configFilePath)
             if (!customConfig.exists()) {
                 logger.info("Custom config $configFilePath not found, using default scalafmt config")
-                return ScalafmtConfig$.MODULE$.default
             }
         }
-        def parsedConfig = Config.fromHoconString(customConfig.text, Config.fromHoconString$default$2())
-
-        try {
-            parsedConfig.get()
-        } catch(Exception e) {
-            logger.warn("Unable to parse .scalafmt.conf in the project root. The format of the file is incorrect. Exception: $e")
-            ScalafmtConfig$.MODULE$.default
-        }
+        return customConfig
     }
 }
