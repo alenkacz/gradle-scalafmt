bf38ec453d7895b7efce1cc89e2596084bff4d8e
boris <smidtboris1@gmail.com>
2019-03-05 23:34:22 +0100

    added git based diff check to only format changed files

--- src/main/groovy/cz/alenkacz/gradle/scalafmt/ScalafmtFormatBase.groovy
+++ src/main/groovy/cz/alenkacz/gradle/scalafmt/ScalafmtFormatBase.groovy
@@ -1,7 +1,6 @@
 package cz.alenkacz.gradle.scalafmt
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.tasks.SourceSet
 import org.scalafmt.interfaces.Scalafmt
@@ -16,15 +15,14 @@ class ScalafmtFormatBase extends DefaultTask {
             .withRespectVersion(false)
             .withDefaultVersion("1.5.1")
 
+
     def runScalafmt(boolean testOnly = false) {
         if (project.plugins.withType(JavaBasePlugin).empty) {
             logger.info("Java or Scala gradle plugin not available in this project, nothing to format")
             return
         }
-        def configpath = ConfigFactory.get(logger,project,pluginExtension.configFilePath)
 
-
-        def misformattedFiles = formatFiles(sourceSet.allSource.getFiles(),testOnly)
+        formatFiles(sourceSet.allSource.getFiles(),testOnly)
 
         if (testOnly && !misformattedFiles.empty) {
             throw new ScalafmtFormatException(misformattedFiles)
@@ -32,8 +30,9 @@ class ScalafmtFormatBase extends DefaultTask {
     }
 
     def formatFiles(Set<File> files, testOnly = false) {
+        def configpath = ConfigFactory.get(logger,project,pluginExtension.configFilePath)
         def misformattedFiles = new ArrayList<String>()
-        files.filter { File f -> canBeFormatted(f) }.each { File f ->
+        files.findAll { File f -> canBeFormatted(f) }.each { File f ->
             String contents = f.text
             logger.debug("Formatting '$f'")
             def formattedContents = formatter.format(configpath.toPath(), f.toPath(), contents)
